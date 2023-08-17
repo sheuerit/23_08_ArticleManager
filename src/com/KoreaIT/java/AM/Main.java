0a74c89b176c596f3216edcdbc0af494ae31fedb
sheuerit <sheuerit@gmail.com>
2023-08-17 14:42:30 +0900

    게시물 작성 기능 구현

--- src/com/KoreaIT/java/AM/Main.java
+++ src/com/KoreaIT/java/AM/Main.java
@@ -6,16 +6,41 @@ public class Main {
     System.out.println("== 프로그램 시작 ==");
     Scanner sc = new Scanner(System.in);
 
-    System.out.printf("명령어 ) ");
+    int lastArticleid = 0;
 
-    String cmd = sc.nextLine();
-    System.out.printf("입력된 명령어 : %s\n", cmd);
+    while(true) {
+      System.out.printf("명령어 ) ");
+      String cmd = sc.nextLine();
 
-    int num = sc.nextInt();
-    System.out.printf("입력된 정수 : %d\n", num);
+      if (cmd.length() == 0) {
+        System.out.println("명령어를 입력하세요.");
+        continue;
+      }
+
+      if(cmd.equals("system exit")) {
+        break;
+      }
+
+      if(cmd.equals("article write")) {
+        int id = lastArticleid + 1;
+        lastArticleid = id;
+
+        System.out.printf("제목 : ");
+        String title = sc.nextLine();
+        System.out.printf("내용 : ");
+        String body = sc.nextLine();
+
+        System.out.printf("%d번 글이 생성 되었습니다.\n", id);
+
+      } else if (cmd.equals("article list")) {
+        System.out.println("게시글이 없습니다.");
+      } else {
+        System.out.println("존재하지 않는 명령어 입니다.");
+        continue;
+      }
+    }
 
     sc.close();
-
     System.out.println("== 프로그램 종료 ==");
   }
 }
