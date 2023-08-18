e44260170460409b0bf91614dddb8f4cf36f7ed9
sheuerit <sheuerit@gmail.com>
2023-08-18 10:50:54 +0900

    게시물 상세페이지 구현(날짜 아직)

--- src/com/KoreaIT/java/AM/Main.java
+++ src/com/KoreaIT/java/AM/Main.java
@@ -38,6 +38,31 @@ public class Main {
 
         System.out.printf("%d번 글이 생성 되었습니다.\n", id);
 
+      } else if (cmd.startsWith("article detail")) {
+        String[] cmdBits = cmd.split(" ");
+        int id = Integer.parseInt(cmdBits[2]);
+
+        Article foundArticle = null;
+
+        for (int i = 0; i < articles.size(); i++) {
+          Article article = articles.get(i);
+          if (article.id == id) {
+            foundArticle = article;
+            break;
+          }
+        }
+
+        if (foundArticle == null) {
+          System.out.printf("%d번 게시글은 존재하지 않습니다.\n", id);
+          continue;
+        }
+
+        System.out.printf("번호 : %d\n", foundArticle.id);
+        System.out.printf("날짜 : 2023-12-09 12:12:12\n");
+        System.out.printf("제목 : %s\n", foundArticle.title);
+        System.out.printf("내용 : %s\n", foundArticle.body);
+
+
       } else if (cmd.equals("article list")) {
         if (articles.size() == 0) {
           System.out.println("게시글이 없습니다.");
