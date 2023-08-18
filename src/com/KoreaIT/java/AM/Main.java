b2184334355947a3ac45471d4e783382e177bd79
sheuerit <sheuerit@gmail.com>
2023-08-18 13:50:24 +0900

    게시물 수정 기능 구현

--- src/com/KoreaIT/java/AM/Main.java
+++ src/com/KoreaIT/java/AM/Main.java
@@ -64,6 +64,35 @@ public class Main {
         articles.remove(foundIdx);
         System.out.printf("%d번 글이 삭제 되었습니다.\n", id);
 
+      } else if (cmd.startsWith("article modify")) {
+        String[] cmdBits = cmd.split(" ");
+        int id = Integer.parseInt(cmdBits[2]);
+
+        Article foundArticle = null;
+
+        for (int i = 0; i < articles.size(); i++) {
+          Article article = articles.get(i);
+          if (article.id == id) {
+            foundArticle = article;
+            break;
+          }
+        }
+
+        if (foundArticle == null) {
+          System.out.printf("%d번 게시글은 존재하지 않습니다.\n", id);
+          continue;
+        }
+
+        System.out.printf("제목 : ");
+        String title = sc.nextLine();
+        System.out.printf("내용 : ");
+        String body = sc.nextLine();
+
+        foundArticle.title = title;
+        foundArticle.body = body;
+
+        System.out.printf("%d번 글이 수정 되었습니다.\n", id);
+
       } else if (cmd.startsWith("article detail")) {
         String[] cmdBits = cmd.split(" ");
         int id = Integer.parseInt(cmdBits[2]);
