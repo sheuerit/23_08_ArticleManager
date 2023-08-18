d2d8a28dffc2e31a18187d8555711a4f320509ae
sheuerit <sheuerit@gmail.com>
2023-08-18 14:50:59 +0900

    테스트데이터 생성 메서드 구현

--- src/com/KoreaIT/java/AM/Main.java
+++ src/com/KoreaIT/java/AM/Main.java
@@ -4,12 +4,16 @@ import java.util.List;
 import java.util.Scanner;
 
 public class Main {
+  private static List<Article> articles;
+
+  static {
+    articles = new ArrayList<>();
+  }
+
   public static void main(String[] args) {
     System.out.println("== 프로그램 시작 ==");
+    makeTestData();
     Scanner sc = new Scanner(System.in);
-
-    int lastArticleid = 0;
-    List<Article> articles = new ArrayList<>();
 
     while(true) {
       System.out.printf("명령어 ) ");
@@ -25,8 +29,7 @@ public class Main {
       }
 
       if(cmd.equals("article write")) {
-        int id = lastArticleid + 1;
-        lastArticleid = id;
+        int id = articles.size() + 1;
 
         String regDate = Util.getNowDateStr();
         System.out.printf("제목 : ");
@@ -127,7 +130,7 @@ public class Main {
           System.out.println("번호  |  제목  |  조회수");
           for (int i = articles.size() - 1; i >= 0; i--) {
             Article article = articles.get(i);
-            System.out.printf("%2d    |    %2s    |   %3d\n", article.id, article.title, article.viewCnt);
+            System.out.printf("%4d    |    %2s    |   %3d\n", article.id, article.title, article.viewCnt);
           }
         }
 
@@ -141,6 +144,14 @@ public class Main {
     sc.close();
     System.out.println("== 프로그램 종료 ==");
   }
+
+  private static void makeTestData() {
+    System.out.println("테스트데이터를 생성합니다");
+
+    articles.add(new Article(1, Util.getNowDateStr(), "title1", "body1", 11));
+    articles.add(new Article(2, Util.getNowDateStr(), "title2", "body2", 22));
+    articles.add(new Article(3, Util.getNowDateStr(), "title3", "body3", 33));
+  }
 }
 
 class Article {
@@ -151,11 +162,15 @@ class Article {
   int viewCnt;
 
   Article(int id, String regDate, String title, String body) {
+    this(id, regDate, title, body, 0);
+  }
+
+  Article(int id, String regDate, String title, String body, int viewCnt) {
     this.id = id;
     this.regDate = regDate;
     this.title = title;
     this.body = body;
-    this.viewCnt = 0;
+    this.viewCnt = viewCnt;
   }
 
   public void increaseViewCnt(){
